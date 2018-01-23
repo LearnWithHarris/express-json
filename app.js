ceb7686ddcf28277a4178e1f09bc917bf1d221d7
Heraldo Yusron <heraldoyusrontris@gmail.com>
2018-01-23 18:12:50 +0700

    done

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res)=>{
     // })
 })
 
-//CONTACT 
+//CONTACT ========================================
 app.get('/contacts', (req, res)=>{
     db.all(`SELECT * FROM Contact`, (err, row)=>{
         if(!err){
@@ -51,6 +51,105 @@ app.get('/contacts/edit/:id', (req, res)=>{
         }
     })
 })
-app.post('/contacts/edit/:id')
+app.post('/contacts/edit/:id', (req, res)=>{
+    db.all(`UPDATE Contact
+    SET name = "${req.body.name}", company = "${req.body.company}", phone = ${req.body.phone}, email = "${req.body.email}" 
+    WHERE id = ${req.params.id}`,(err, row)=>{
+        if(!err){
+            res.redirect('/contacts')
+        }else{
+            console.log(err);
+        }
+    })
+})
+//                                          DELETE
+app.get('/contacts/delete/:id', (req, res)=>{
+    db.all(`DELETE FROM Contact
+    WHERE id = ${req.params.id} `,(err, row)=>{
+        if(!err){
+            res.redirect('/contacts')
+        }else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//ADDRESS =========================================
+app.get('/addresses', (req, res)=>{
+    db.all(`SELECT Address.*, Contact.name 
+    FROM Address
+    LEFT JOIN Contact
+    ON Address.contact_id = Contact.id
+    `, (errAddress, rowAddress)=>{
+        if(!errAddress){
+            db.all(`SELECT * FROM Contact`, (errContact, rowContact)=>{
+                if(!errContact){
+                    res.render('address', {contact: rowContact,address: rowAddress})
+                }else{
+                    console.log(errContact)
+                }
+            })
+        }else{
+            console.log(errAddress)
+        }
+    })
+})
+// app.get('/addresses', (req, res)=>{
+//     db.all(`SELECT * FROM Address`, (err, row)=>{
+//         if(!err){
+//             res.render('address', {address: row})
+//             // res.send(row)
+//             // console.log(row.length)
+//         }else{
+//             console.log(err)
+//         }
+//     })
+// })
+//                                          INSERT
+app.post('/addresses', (req, res)=>{
+    db.run(`INSERT INTO Address(street, city, zip_code, contact_id)
+    VALUES("${req.body.street}", "${req.body.city}", ${req.body.zip_code}, ${req.body.contact_id})`, (err, row)=>{
+        if(!err){
+            res.redirect('/addresses')
+        }else{
+            console.log(err);
+        }
+    })
+})
+//                                          EDIT
+app.get('/addresses/edit/:id', (req, res)=>{
+    db.all(`SELECT * FROM Address WHERE id = ${req.params.id}`,(err, row)=>{
+        if(!err){
+            res.render('addressEdit', {address: row})
+            // res.send(`${req.params.id} ini ngetest`);
+        }else{
+            console.log(err)
+        }
+    })
+})
+app.post('/addresses/edit/:id', (req, res)=>{
+    db.all(`UPDATE Address
+    SET street = "${req.body.street}", city = "${req.body.city}", zip_code = ${req.body.zip_code},  contact_id = ${req.body.contact_id} 
+    WHERE id = ${req.params.id}`,(err, row)=>{
+        if(!err){
+            res.redirect('/addresses')
+        }else{
+            console.log(err);
+        }
+    })
+})
+//                                          DELETE
+app.get('/addresses/delete/:id', (req, res)=>{
+    db.all(`DELETE FROM Address
+    WHERE id = ${req.params.id} `,(err, row)=>{
+        if(!err){
+            res.redirect('/addresses')
+        }else{
+            console.log(err)
+        }
+    })
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
